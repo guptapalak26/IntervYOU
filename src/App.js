49fa8aaaf120047422984e6979a99ae77d0001e1
Sahil Gupta <sahilgupta70500@gmail.com>
2025-01-17 22:26:37 -0500

    Front-end
    This is only the frontend portion of the our project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is beginning of our UOFT hacks project
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
